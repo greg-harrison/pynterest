1ed9a3933c4627ac79718374f1a48f8ba0b89229
Greg <greg@Gregs-MacBook-Pro.local>
2018-01-25 07:28:42 -0500

    Hello World moment for this method. I was able to receive the response object on the other side, very simply as well.

--- server/app.py
+++ server/app.py
@@ -1,10 +1,18 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
+test = {
+    0: 'hello',
+    1: 'world',
+    2: 'test',
+}
+
 @app.route('/')
 def index():
-    return 'INDEX'
+    response = jsonify(test)
+    response.status_code = 200
+    return response
 
 if __name__ == '__main__':
     app.run()
