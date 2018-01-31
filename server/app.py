d077e8e899f6520cdb6fe96eff55d8ca8e50f038
Greg <greg@Gregs-MacBook-Pro.local>
2018-01-31 07:41:11 -0500

    Added very basic user authentication - authorized route

--- server/app.py
+++ server/app.py
@@ -1,15 +1,34 @@
 from flask_api import FlaskAPI, status
+from flask_httpauth import HTTPBasicAuth
 from flask_cors import CORS
 import json
 
 app = FlaskAPI(__name__)
 CORS(app)
+auth = HTTPBasicAuth()
 
 users = json.loads(open('mockdata.json').read())
+secretData = json.loads(open('mocksecretdata.json').read())
+
+USERDATA = {'admin': 'password'}
+
+
+@auth.verify_password
+def verify(username, password):
+    if not (username and password):
+        return False
+    return USERDATA.get(username) == password
+
 
 @app.route('/')
 def index():
     return users, status.HTTP_200_OK
+
+
+@app.route('/secret')
+@auth.login_required
+def secretIndex():
+    return secretData, status.HTTP_200_OK
 
 
 if __name__ == '__main__':
