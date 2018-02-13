64ed3c5b9856c3b414bf25f14e021ef7cb1e687a
Greg <greg@Gregs-MacBook-Pro.local>
2018-02-13 07:38:35 -0500

    Updates to the front-end components

---
+++ pynterest-client/src/components/base_components/pyn-container.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="pyn-container">
+    <span>
+    -- Infinitely scroll and build out pyn-card components  --
+    </span>
+    <PynCard />
+
+  </div>
+</template>
+
+<script>
+import PynCard from "../pyn_components/pyn-card";
+
+export default {
+  components: {
+    PynCard
+  }
+};
+</script>
+
+<style lang="scss">
+.pyn-container {
+  display: inline-flex;
+  margin-top: 3rem;
+}
+</style>
