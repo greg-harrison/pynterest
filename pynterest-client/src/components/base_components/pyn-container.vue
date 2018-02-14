c4975817d5b9d64d991be4a87a2060e86b797044
Greg <greg@Gregs-MacBook-Pro.local>
2018-02-14 07:40:36 -0500

    Built out the first card components using mock data

--- pynterest-client/src/components/base_components/pyn-container.vue
+++ pynterest-client/src/components/base_components/pyn-container.vue
@@ -1,10 +1,11 @@
 <template>
   <div class="pyn-container">
-    <span>
-    -- Infinitely scroll and build out pyn-card components  --
-    </span>
-    <PynCard />
-
+    <PynCard
+      v-for="(pyn, index) in pyns"
+      v-bind:pyn="pyn"
+      v-bind:index="index"
+      v-bind:key="pyn.pyn_id"
+    />
   </div>
 </template>
 
@@ -12,6 +13,33 @@
 import PynCard from "../pyn_components/pyn-card";
 
 export default {
+  data() {
+    return {
+      pyns: [
+        {
+          pyn_id: "b93bfdcd-3507-40d6-9778-71d008197b9d",
+          title: "Test",
+          url: "http://www.testing.org",
+          description: "A test",
+          imageUrl: "http://via.placeholder.com/236x150"
+        },
+        {
+          pyn_id: "b93bfdcd-3507-40d6-9778-71d008197b9c",
+          title: "Test",
+          url: "http://www.testing.org",
+          description: "A test",
+          imageUrl: "http://via.placeholder.com/236x150"
+        },
+        {
+          pyn_id: "b93bfdcd-3507-40d6-9778-71d008197b9e",
+          title: "Test",
+          url: "http://www.testing.org",
+          description: "A test",
+          imageUrl: "http://via.placeholder.com/236x150"
+        }
+      ]
+    };
+  },
   components: {
     PynCard
   }
