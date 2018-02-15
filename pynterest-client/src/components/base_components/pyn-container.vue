ebe20d8896d8433f4fd7b208207996c5fe97f143
Greg <greg@Gregs-MacBook-Pro.local>
2018-02-15 07:23:58 -0500

    back to normal and some good lessons learned

--- pynterest-client/src/components/base_components/pyn-container.vue
+++ pynterest-client/src/components/base_components/pyn-container.vue
@@ -39,13 +39,6 @@ export default {
           imageUrl: "http://via.placeholder.com/236x150"
         },
         {
-          pyn_id: "b93bfdcd-3507-40d6-9778-71d008197b9f",
-          title: "Test",
-          url: "http://www.testing.org",
-          description: "A test",
-          imageUrl: "http://via.placeholder.com/236x250"
-        },
-        {
           pyn_id: "b93bfdcd-3507-40d6-9778-71d008197b9g",
           title: "Test",
           url: "http://www.testing.org",
